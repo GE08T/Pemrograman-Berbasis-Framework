1977da12ebe91b7ac9961e85b222679f99e0db16
GE08T <gaerthamgk@gmail.com>
2026-02-23 20:43:26 +0700

    Pertemuan 2_Jobsheet 4 : Optional Catch-All Route

--- Pertemuan 2/link-navigation/src/pages/shop/[[...slug]].tsx
+++ Pertemuan 2/link-navigation/src/pages/shop/[[...slug]].tsx
@@ -4,11 +4,13 @@ const halamanToko = () => {
   // const router = useRouter();
   // console.log(router);
   const { query } = useRouter();
+  const slug = query.slug;
+
   return (
     <div>
       <h1>Halaman Toko</h1>
-      <p>Toko : {`${query.slug && query.slug[0] + "-" + query.slug[1]}`}</p>
-      {/* <p>Toko : {Array.isArray(query.slug) ? query.slug.join("-") : query.slug}</p> */}
+      {/* <p>Toko : {`${query.slug && query.slug[0] + "-" + query.slug[1]}`}</p> */}
+      <p>Toko : {Array.isArray(query.slug) ? query.slug.join("-") : query.slug}</p>
     </div>
   );
 };
