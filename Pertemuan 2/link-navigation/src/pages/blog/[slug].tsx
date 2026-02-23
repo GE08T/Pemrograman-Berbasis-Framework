28b4bebf6b0261b28bdf801409298f6feee0aa64
GE08T <gaerthamgk@gmail.com>
2026-02-23 20:28:01 +0700

    Pertemuan 2_Jobsheet 4 : Membuat Catch-All Route

---
+++ Pertemuan 2/link-navigation/src/pages/blog/[slug].tsx
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router';
+
+const BlogPage = () => {
+
+  const { query } = useRouter();
+  return (
+    <div>
+      <h1>Halaman Blog</h1>
+      <p>Blog : {query.slug}</p>
+    </div>    
+  );
+}
+
+export default BlogPage;
---
+++ Pertemuan 2/link-navigation/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Catch-All Routing, Optional Catch-All, Linking & Navigating pada Next.js Pages Router</h1>
+      <p>Mahasiswa D4 Pemrograman Berbasis Framework</p>
+    </div>
+  )
+}
