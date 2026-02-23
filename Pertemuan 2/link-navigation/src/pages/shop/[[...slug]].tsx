6859dc5cd106aa9ac55c45f742cb7bb644db6be3
GE08T <gaerthamgk@gmail.com>
2026-02-23 20:44:05 +0700

    Pertemuan 2_Jobsheet 4 : Validasi Parameter

--- Pertemuan 2/link-navigation/src/pages/shop/[[...slug]].tsx
+++ Pertemuan 2/link-navigation/src/pages/shop/[[...slug]].tsx
@@ -10,7 +10,8 @@ const halamanToko = () => {
     <div>
       <h1>Halaman Toko</h1>
       {/* <p>Toko : {`${query.slug && query.slug[0] + "-" + query.slug[1]}`}</p> */}
-      <p>Toko : {Array.isArray(query.slug) ? query.slug.join("-") : query.slug}</p>
+      {/* <p>Toko : {Array.isArray(query.slug) ? query.slug.join("-") : query.slug}</p> */}
+      <p>Kategori: {slug ? slug[0] : "Semua Kategori"}</p>
     </div>
   );
 };
