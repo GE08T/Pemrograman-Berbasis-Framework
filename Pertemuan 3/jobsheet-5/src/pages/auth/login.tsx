72e8b8a65f44f612cce662dc5c58b423733c10cc
GE08T <gaerthamgk@gmail.com>
2026-03-03 03:25:45 +0700

    Pertemuan 3_Jobsheet 5 : Styling untuk Pages (CSS Module)

--- Pertemuan 3/jobsheet-5/src/pages/auth/login.tsx
+++ Pertemuan 3/jobsheet-5/src/pages/auth/login.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import handler from "../api/hello";
+import styles from "./login.module.css";
 
 const halamanLogin = () => {
   const { push } = useRouter();
@@ -9,7 +9,7 @@ const halamanLogin = () => {
     push('/produk');
   }
   return (
-    <div>
+    <div className={styles.login}>
       <h1>Halaman Login</h1>
       <button onClick={handlerLogin}>Login</button>  <br />
       {/* <button onClick={() => push('/produk')}>Login</button>  <br />
