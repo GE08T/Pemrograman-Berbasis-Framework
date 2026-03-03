143822d089a9b71d91bc3f9458c87a6c6d051ac4
GE08T <gaerthamgk@gmail.com>
2026-03-04 00:58:43 +0700

    Pertemuan 3_Jobsheet 9 : Setup Halaman SSR

---
+++ Pertemuan 3/jobsheet-9/src/pages/utils/db/servicefirebase.ts
@@ -0,0 +1,13 @@
+import { collection, getDocs, getFirestore} from "firebase/firestore";
+import app from "./firebase";
+
+const db = getFirestore(app);
+
+export async function retriveProducts(collectionName: string) {
+  const snapshot = await getDocs(collection(db, collectionName));
+  const data = snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  return data;
+}
