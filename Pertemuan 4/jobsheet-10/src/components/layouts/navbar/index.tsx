f44e0c5187c6e1444bc8c9a8be74a63e213fc689
GE08T <gaerthamgk@gmail.com>
2026-03-10 21:00:54 +0700

    Pertemuan 4_Jobsheet 10 : Setup Halaman Static

---
+++ Pertemuan 4/jobsheet-10/src/components/layouts/navbar/index.tsx
@@ -0,0 +1,14 @@
+import styles from "./navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <div className={styles.navbar}>
+      <div className="big">
+        Navbar Component
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
+
