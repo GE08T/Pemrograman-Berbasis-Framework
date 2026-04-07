c3008a50ffc62f08a6fd121303e55b45bf5ab72e
GE08T <gaerthamgk@gmail.com>
2026-04-07 20:07:05 +0700

    Pertemuan 5_Jobsheet 15 : Membuat API Register

--- Pertemuan 5/jobsheet-15/src/views/auth/register/index.tsx
+++ Pertemuan 5/jobsheet-15/src/views/auth/register/index.tsx
@@ -1,7 +1,46 @@
 import Link from "next/link";
 import style from "./register.module.scss";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 const TampilanRegister = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { push } = useRouter();
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email") as string;
+    const fullname = formData.get("Fullname") as string;
+    const password = formData.get("Password") as string;
+
+    setIsLoading(true); // Menambahkan ini agar loading state berjalan
+
+    const response = await fetch("/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, fullname, password }),
+    });
+
+    // const result = await response.json();
+    // console.log(result);
+
+    if (response.status === 200) {
+      form.reset();
+      // event.currentTarget.reset();
+      setIsLoading(false);
+      push("/auth/login");
+    } else {
+      setIsLoading(false);
+      setError(
+        response.status === 400 ? "User already exists" : "An error occurred",
+      );
+    }
+  };
   return (
     <div className={style.register}>
       <h1 className={style.register__title}>Halaman Register</h1>
