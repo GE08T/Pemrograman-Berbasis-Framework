8d2efc419c3a96c4f78d44db943af1d02826ae25
GE08T <gaerthamgk@gmail.com>
2026-04-07 22:04:20 +0700

    Pertemuan 6_Jobsheet 16 :  Handle Login di Frontend

--- Pertemuan 6/jobsheet-16/src/views/auth/login/index.tsx
+++ Pertemuan 6/jobsheet-16/src/views/auth/login/index.tsx
@@ -1,20 +1,97 @@
 import Link from "next/link";
+import style from "./login.module.scss";
+import { useState } from "react";
 import { useRouter } from "next/router";
-// import styles from "./login.module.css";
-import styles from "./login.module.scss";
+import { signIn } from "next-auth/react"; // Tambahkan import ini
 
 const TampilanLogin = () => {
-  const { push } = useRouter();
-  const handlerLogin = () => {
-    // logic login disini
-    push('/produk');
-  }
+  const [isLoading, setIsLoading] = useState(false);
+  const { push, query } = useRouter(); // Tambahkan query dari useRouter
+  const [error, setError] = useState("");
+
+  // Ambil callbackUrl dari query string, default ke "/"
+  const callbackUrl: any = query.callbackUrl || "/";
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    setError("");
+    setIsLoading(true);
+
+    // const form = event.currentTarget;
+    // const formData = new FormData(form);
+    // const email = formData.get("email") as string;
+    // const fullname = formData.get("Fullname") as string;
+    // const password = formData.get("Password") as string;
+
+    try {
+      // Menggunakan NextAuth signIn
+      const res = await signIn("credentials", {
+        redirect: false,
+        email: event.target.email.value,
+        password: event.target.password.value,
+        callbackUrl,
+      });
+
+      if (!res?.error) {
+        setIsLoading(false);
+        push(callbackUrl);
+      } else {
+        setIsLoading(false);
+        setError(res?.error || "Login failed");
+      }
+    } catch (error) {
+      setIsLoading(false);
+      setError("wrong email or password");
+    }
+  };
   return (
-    <div className={styles.login}>
-      <h1 className="text-3xl font-bold text-blue-600">Halaman Login</h1>
-      <button onClick={handlerLogin}>Login</button>  <br />
-      <h1 style={{color:"red", border:"1px solid red", borderRadius:"5px", padding:"5px"}}> belum punya akun </h1>
-      <Link href="/auth/register">Ke Halaman Register</Link>
+    <div className={style.login}>
+      {error && <p className={style.login__error}>{error}</p>}
+      <h1 className={style.login__title}>Halaman Login</h1>
+      <div className={style.login__form}>
+        <form onSubmit={handleSubmit}>
+          <div className={style.login__form__item}>
+            <label htmlFor="email" className={style.login__form__item__label}>
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+              className={style.login__form__item__input}
+            />
+          </div>
+
+          <div className={style.login__form__item}>
+            <label
+              htmlFor="password"
+              className={style.login__form__item__label}
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              className={style.login__form__item__input}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className={style.login__form__item__button}
+            disabled={isLoading}
+          >
+            {isLoading ? "Loading..." : "Login"}
+          </button>
+        </form>
+        <br />
+        <p className={style.login__form__item__text}>
+          Sudah punya akun? <Link href="/auth/login">Ke Halaman Login</Link>
+        </p>
+      </div>
     </div>
   );
 };
