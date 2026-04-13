d042b3571759614d19af4ff869725766fe7f08da
GE08T <gaerthamgk@gmail.com>
2026-04-13 21:01:43 +0700

    Pertemuan 6_Jobsheet 19 : Konfigurasi Coverage Lengkap

--- Pertemuan 6/jobsheet-19/src/__test__/pages/about.spec.tsx
+++ Pertemuan 6/jobsheet-19/src/__test__/pages/about.spec.tsx
@@ -1,9 +1,10 @@
 import { render } from "@testing-library/react";
+import { describe, expect, it } from "@jest/globals";
 import AboutPage from "../../pages/about";
 
 describe("About Page", () => {
   it("should render the about page correctly", () => {
     const page = render(<AboutPage />);
     expect(page).toMatchSnapshot();
-  })
-})
+  });
+});
